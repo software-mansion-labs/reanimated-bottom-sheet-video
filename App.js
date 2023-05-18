749a1408e2494e474049d4eceeeed00436ec6633
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-05-18 16:56:49 +0200

    Draft example

--- App.js
+++ App.js
@@ -1,46 +1,92 @@
+import "react-native-gesture-handler";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 import Animated, {
-  useSharedValue,
-  withTiming,
+  SlideInDown,
+  SlideOutDown,
   useAnimatedStyle,
-  Easing,
+  useSharedValue,
+  withSpring,
+  runOnJS,
 } from "react-native-reanimated";
-import { View, Button } from "react-native";
+import { Easing } from "react-native-reanimated";
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+const HEIGHT = 300;
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
+function App() {
+  const [isOpen, setOpen] = useState(true);
+
+  const offset = useSharedValue(0);
+
+  const toggleSheet = () => {
+    setOpen(!isOpen);
+    offset.value = 0;
   };
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
+  const pan = Gesture.Pan()
+    .onChange((event) => {
+      const offsetDelta = event.changeY + offset.value;
+
+      offset.value =
+        offsetDelta > 0 ? offsetDelta : Math.min(offset.value, -10);
+    })
+    .onFinalize(() => {
+      if (offset.value < HEIGHT / 3) {
+        offset.value = withSpring(0);
+      } else {
+        runOnJS(toggleSheet)();
+      }
+    });
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateY: offset.value }],
+  }));
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <Button title="Toggle Sheet" onPress={toggleSheet} />
+
+      {isOpen && (
+        <GestureDetector gesture={pan}>
+          <Animated.View
+            style={[styles.sheet, animatedStyles]}
+            entering={SlideInDown.springify().damping(15)}
+            exiting={SlideOutDown}
+          >
+            <Text style={styles.label}>Hello World!</Text>
+          </Animated.View>
+        </GestureDetector>
+      )}
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#eee",
+  },
+  label: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  sheet: {
+    backgroundColor: "white",
+    padding: 16,
+    height: HEIGHT,
+    width: "100%",
+    position: "absolute",
+    bottom: -20,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+});
+
+export default App;
